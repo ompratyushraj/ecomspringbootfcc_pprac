aa3a1fcb4426e096cdaf95431da1c1ed0bbc0ae1
Pratyush Raj Srivastava <ompratyushraj@gmail.com>
2024-11-12 22:35:08 +0530

    API error handling 02

--- src/main/java/com/fccpractice/ecomfreecodecamp/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/fccpractice/ecomfreecodecamp/security/jwt/AuthTokenFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,9 +19,11 @@ import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-    private JwtUtils jwtUtils;
-    private ShopUserDetailsService userDetailsService;
+    @Autowired
+    private JwtUtils jwtUtils; // Inject JwtUtils here
 
+    @Autowired
+    private ShopUserDetailsService userDetailsService; // Inject ShopUserDetailsService here
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
@@ -28,7 +31,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
             String jwt = parseJwt(request);
-            if(StringUtils.hasText(jwt) && jwtUtils.validateToken(jwt)){
+            if (StringUtils.hasText(jwt) && jwtUtils.validateToken(jwt)) {
                 String username = jwtUtils.getUsernameFromToken(jwt);
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
@@ -38,7 +41,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write(e.getMessage() + " : Invalid or expired token, you may login and try again !");
             return;
-        } catch (Exception e){
+        } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.getWriter().write(e.getMessage());
             return;
@@ -46,9 +49,9 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String parseJwt(HttpServletRequest request){
+    private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
-        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")){
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7);
         }
         return null;
